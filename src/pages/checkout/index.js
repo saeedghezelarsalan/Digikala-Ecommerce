284ece702f8b1308966f5db9e993cff4aaf6f437
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-02-21 02:55:53 +0330

    basket cart page

---
+++ src/pages/checkout/index.js
@@ -0,0 +1,15 @@
+const index = () => {
+    return <></>;
+  };
+  
+  export default index;
+  
+  export async function getServerSideProps({ params }) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+  
