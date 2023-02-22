f068db9a9146a68abe121dc9ee67e2a3b6055854
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-02-22 03:41:25 +0330

    navbar

---
+++ src/components/SidebarCategoryNav.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import SidebarSubCategoryNav from "./SidebarSubCategoryNav";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Link from "next/link";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+
+const SidebarCategoryNav = ({ mainCategory, index, category }) => {
+  const [openMobileNavbarCategory, setOpenMobileNavbarCategory] =
+    useState(false);
+
+  const toggleMobileNavbar = (id, e) => {
+    if (openMobileNavbarCategory == e) {
+      setOpenMobileNavbarCategory(null);
+    }
+    setOpenMobileNavbarCategory((e) => !e);
+  };
+
+  return (
+    <div className="">
+      <div
+        className={`px-8 w-full flex justify-between items-center cursor-pointer ${openMobileNavbarCategory ? "text-[#ef4056]" : "text-black"} `}
+        onClick={() => toggleMobileNavbar(index)}
+        key={index}
+      >
+        <span className="text-[13px] flex items-center h-[52px]">{mainCategory.name}</span>
+        <KeyboardArrowDownIcon />
+      </div>
+
+
+      
+
+
+      <div className={`${openMobileNavbarCategory ? "block" : "hidden"}`}>
+        {category
+          .filter((category) => category.mainCategory == mainCategory.name)
+          .map((category, indexs) => (
+            <div className="bg-[#f1f2f4]">
+            <Link href={`/main/${mainCategory.slug}`}>
+          <a className="py-4 flex items-center px-8 mx-8">
+            <span className="text-xs text-[#9e9fb1]">همه موارد این دسته</span>
+            <KeyboardArrowLeftIcon className="fill-[#9e9fb1] w-4 h-4" />
+          </a>
+        </Link>
+            <SidebarSubCategoryNav mainCategorySlug={mainCategory.slug} category={category} indexs={indexs} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SidebarCategoryNav;
